7f451cf93b4be1e1af8e9d5505c32fda906bd95f
Merel Kokkeler <kokkelermerel@gmail.com>
2017-01-11 11:18:35 +0100

    Figuur 2

---
+++ PROG5-LeagueOfNinjas/View/Ninjas/NinjaVisualView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROG5_LeagueOfNinjas.View.Ninjas
+{
+    /// <summary>
+    /// Interaction logic for NinjaVisualView.xaml
+    /// </summary>
+    public partial class NinjaVisualView : Window
+    {
+        public NinjaVisualView()
+        {
+            InitializeComponent();
+        }
+    }
+}
